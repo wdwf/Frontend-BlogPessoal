421648f42cacd7d0bd376cde6dd7fb1a2ae90bb6
wdwf <w_dwf@hotmail.com>
2025-05-21 13:05:18 -0300

    :art: Add icons e cria navbar e footer alem de estilizar os componentes com tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router'
 import './App.css'
+import Footer from './components/footer/Footer'
+import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <Home />
+    <div className="min-h-screen flex flex-col">
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
   )
 }
 
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,40 +1,37 @@
-import React from 'react'
 
 function Home() {
-    return (
-        <>
-            <div style={{
-                width: "100%",
-                display: "flex",
-                justifyContent: "center"
-            }}>
-                <div>
-                    <div style={{
-                        width: "80vw",
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center"
-                    }}>
-                        <h2>Seja Bem Vinde!</h2>
-                        <p>Expresse aqui seus pensamentos e opniões</p>
-                    </div>
+	return (
+		<>
+			<div className="flex-grow bg-indigo-900 flex justify-center">
+				<div className='container grid grid-cols-2 text-white'>
+					<div className="flex flex-col gap-4 items-center justify-center py-4">
+						<h2 className='text-5xl font-bold'>
+							Seja Bem Vinde!
+						</h2>
+						<p className='text-xl'>
+							Expresse aqui seus pensamentos e opniões
+						</p>
 
-                    <div style={{
-                        width: "80vw",
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center"
-                    }}>
-                        <img
-                            src="https://i.imgur.com/VpwApCU.png"
-                            alt="Imagem da Página Home"
-                            width="400px"
-                        />
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+						<div className="flex justify-around gap-4">
+							<div className='rounded text-white 
+                                            border-white border-solid border-2 py-2 px-4'
+							>
+								Nova Postagem
+							</div>
+						</div>
+					</div>
+
+					<div className="flex justify-center items-center">
+						<img
+							src="https://i.imgur.com/fyfri1v.png"
+							alt="Imagem Página Home"
+							className='h-2/3'
+						/>
+					</div>
+				</div>
+			</div>
+		</>
+	)
 }
 
 export default Home
