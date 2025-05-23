2cfabb8fa50171cafce4e91a685dadf54ca4655c
wdwf <w_dwf@hotmail.com>
2025-05-23 00:30:39 -0300

    :construction: refatora a estrutura de autenticação, adiciona lógica de cadastro e login, e implementa feedback visual de carregamento

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,26 @@ import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 import Cadastro from './pages/cadastro/Cadastro'
 import Login from './pages/login/Login'
+import { AuthProvider } from './contexts/AuthContext'
 
 function App() {
 
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <div>
-          <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/cadastro" element={<Cadastro />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,24 @@
-import { Link } from "react-router"
+import { useContext } from "react";
+import { Link, useNavigate } from "react-router"
+import { AuthContext } from "../../contexts/AuthContext";
 
 function Navbar() {
+
+  const navigate = useNavigate();
+
+  const { handleLogout } = useContext(AuthContext)
+
+  function logout() {
+
+    handleLogout()
+    alert('O Usuário foi desconectado com sucesso!')
+    navigate('/')
+  }
+
   return (
     <>
-      <div className='w-full flex justify-center py-4
-            			   bg-indigo-900 text-white'>
+      <div className='w-full bg-indigo-900 text-white
+                flex justify-center py-4'>
 
         <div className="container flex justify-between text-lg">
           <Link to='/home' className="text-2xl font-bold">Blog Pessoal</Link>
@@ -12,9 +26,9 @@ function Navbar() {
           <div className='flex gap-4'>
             Postagens
             Temas
-            <Link to="/cadastro">Cadastro</Link>
+            Cadastrar tema
             Perfil
-            Sair
+            <Link to='' onClick={logout} className='hover:underline'>Sair</Link>
           </div>
         </div>
       </div>
